e490ecd8d4f194df557912f7aff063a14f3ab2eb
aleksandrovaolena1 <aleksandrovaolena1@gmail.com>
2024-09-23 18:45:21 +0200

    Add ex lesson 1

---
+++ ExeL1.py
@@ -0,0 +1,6 @@
+# Step 1: Ask the user for their name
+name = input("What is your name? ")
+
+# Step 2: Print morning and evening greetings
+print(f"Good morning, {name}")
+print(f"Good evening,{name}!")
